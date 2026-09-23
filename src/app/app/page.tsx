b5ff0a208a4b9426333c79500c6bdd3c7de6d5e2
jubail-jr <hosainjubail8@gmail.com>
2026-09-23 10:48:30 +0600

    Increase App Page Padding

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -8,7 +8,7 @@ const AppPage = async () => {
   const apps: TrendingAppsType[] = await getTrendingApps();
 
   return (
-    <section className="bg-gray-50 py-16 md:py-20">
+    <section className="bg-gray-50 py-25 md:py-30">
       <div className="container mx-auto px-4">
         {/* Section Header */}
         <div className="text-center max-w-2xl mx-auto mb-10 md:mb-12">
